1b333eaadbc1e2ddcf99af7e6b671c7ff5802d74
clfhhc <clfhhc@gmail.com>
2018-03-27 03:59:10 -0700

    finished Advanced

--- data_generator.js
+++ data_generator.js
@@ -16,6 +16,7 @@ window.users = Object.keys(streams.users);
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
   var username = newTweet.user;
+  Object.keys(streams.users).includes(username) ? null : streams.users[username]=[];
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
@@ -58,12 +59,10 @@ scheduleNextTweet();
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
-var writeTweet = function(message){
-  if(!visitor){
-    throw new Error('set the global visitor property!');
-  }
+var writeTweet = function(visitor,message){
   var tweet = {};
   tweet.user = visitor;
   tweet.message = message;
+  tweet.created_at = new Date();
   addTweet(tweet);
 };
